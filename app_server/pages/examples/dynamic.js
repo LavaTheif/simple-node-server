69474f40bb79e063d5e1464c8ff7f68d423d6486
Charlie <charlieferris15@gmail.com>
2020-04-23 22:36:01 +0100

    Update dynamic.js

--- app_server/pages/examples/dynamic.js
+++ app_server/pages/examples/dynamic.js
@@ -9,16 +9,23 @@ exports.dynamic = true;
  @return    An example script.
  */
 
+//Only constants go up here
 const fs = require("fs");
-let template_file = null;
+const template_file = null;
 
+//This function can be async if you need it to be
 exports.eval = function (post) {
     // Load a template file
     // let page = template_file === null?"":fs.readFileSync('./html_templates/' + template_file);
 
-    //generate file completely
+    //generate file completely with no template
+    //Note: you should really use an XML parser
+   //(I just didn't want to add a dependancy for an example)
     let page = "<html>This was generated dynamically <br>";
     page += "current server time: "+new Date().getTime();
     page+="</html>";
+ 
+ //return data should always be in this format.
+ //more info on response codes: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
     return {responseCode: 200, response: page};
 }
